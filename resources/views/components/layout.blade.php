556e8b999c5b8672b7780b7151732bf5f5b2d109
Debora Campopiano <programmazione2023@gmail.com>
2025-04-01 11:06:37 +0200

    Scaffolding

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Creativity</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <x-navbar />  <!--richiamo il componente navbare -->
+
+    <div class="min-vh-100">
+        {{ $slot }}
+    </div>
+
+    <x-footer/>
+
+    <script src="https://kit.fontawesome.com/09c4e03efc.js" crossorigin="anonymous"></script>
+
+</body>
+</html>
